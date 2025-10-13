613bc71aa32fb26acef451f0e23ef69e680c1aee
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-10-13 13:24:31 +0300

    9.1

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,1 +1,22 @@
-const calculateBmi = (height: number, weight: number) => {};
+const calculateBmi = (height: number, weight: number): string => {
+	const result: number = weight / (height / 100) ** 2;
+	if (result < 16) {
+		return "Underweight (Severe thinness)";
+	} else if (result < 17) {
+		return "Underweight (Moderate Thinness)";
+	} else if (result < 18.5) {
+		return "Underweight (Mild Thinness)";
+	} else if (result < 25) {
+		return "Normal Range";
+	} else if (result < 30) {
+		return "Overweight (Pre-Obese)";
+	} else if (result < 35) {
+		return "Obese (Class I)";
+	} else if (result < 40) {
+		return "Obese (Class II)";
+	} else if (result >= 40) {
+		return "Obese (Class III)";
+	}
+};
+
+console.log(calculateBmi(180, 74));
