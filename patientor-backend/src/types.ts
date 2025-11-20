4eeaa1cd151f85848f2837bfd3c1835776b9aa72
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-11-20 09:38:34 +0200

    9.23

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -7,8 +7,47 @@ export interface DiagnosisEntry {
 	latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
+export interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<DiagnosisEntry["code"]>;
+}
+
+export enum HealthRating {
+	"Healthy" = 0,
+	"LowRisk" = 1,
+	"HighRisk" = 2,
+	"CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+	type: "HealthCheck";
+	healthCheckRating: HealthRating;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+	type: "OccupationalHealthcare";
+	employerName: string;
+	sickLeave?: {
+		startDate: string;
+		endDate: string;
+	};
+}
+
+export interface HospitalEntry extends BaseEntry {
+	type: "Hospital";
+	discharge: {
+		date: string;
+		criteria: string;
+	};
+}
+
+export type Entry =
+	| OccupationalHealthcareEntry
+	| HospitalEntry
+	| HealthCheckEntry;
 
 export interface PatientEntry {
 	id: string;
