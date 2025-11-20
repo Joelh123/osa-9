e364513d5cc4d6c16308baa3777867c59a8a6bb4
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-11-20 11:52:57 +0200

    9.25

--- patientor-frontend/src/components/PatientListPage/PatientPage.tsx
+++ patientor-frontend/src/components/PatientListPage/PatientPage.tsx
@@ -1,13 +1,15 @@
 import { useParams } from "react-router-dom";
 import patientService from "../../services/patients";
+import diagnosisService from "../../services/diagnoses";
 import { useEffect, useState } from "react";
-import { Patient } from "../../types";
+import { Diagnosis, Patient } from "../../types";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
 import { QuestionMark } from "@mui/icons-material";
 
 const PatientPage = () => {
 	const [patient, setPatient] = useState<Patient | null>(null);
+	const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
 
 	const id = useParams<{ id: string }>().id;
 
@@ -16,7 +18,12 @@ const PatientPage = () => {
 			const patient = await patientService.getOne(id);
 			setPatient(patient);
 		};
+		const fetchDiagnoses = async () => {
+			const result = await diagnosisService.getAll();
+			setDiagnoses(result);
+		};
 		void fetchPatient();
+		void fetchDiagnoses();
 	}, []);
 	if (!patient) {
 		return <div>Patient not found</div>;
@@ -44,16 +51,22 @@ const PatientPage = () => {
 			<div>occupation: {patient.occupation}</div>
 			<h2>Entries</h2>
 			<div>
-				{patient.entries.map((e) => (
-					<div key={e.id}>
-						{e.date} {e.description}
-						<ul>
-							{e.diagnosisCodes?.map((c) => (
-								<li key={c}>{c}</li>
-							))}
-						</ul>
-					</div>
-				))}
+				{patient.entries.length !== 0 ? (
+					patient.entries.map((e) => (
+						<div key={e.id}>
+							{e.date} {e.description}
+							<ul>
+								{e.diagnosisCodes?.map((c) => (
+									<li key={c}>
+										{c} {diagnoses.find((d) => d.code === c)?.name}
+									</li>
+								))}
+							</ul>
+						</div>
+					))
+				) : (
+					<div>no entries</div>
+				)}
 			</div>
 		</div>
 	);
